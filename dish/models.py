d342144410561ccb9c6cd19493ede9f68550ac2b
Chubak <sopubekov96@gmail.com>
2021-04-28 18:57:56 +0600

    added dish model

--- dish/models.py
+++ dish/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Название блюда'
+        verbose_name_plural = "Название блюд"
+
+    def __str__(self):
+        return self.name
